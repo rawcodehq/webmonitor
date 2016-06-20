4b496e07e894e236df4b5c9ea10ceada4bd2ef39
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2016-06-20 10:48:47 +0530

    make all tests pass

--- test/models/user_test.exs
+++ test/models/user_test.exs
@@ -3,7 +3,7 @@ defmodule Webmonitor.UserTest do
 
   alias Webmonitor.User
 
-  @valid_attrs %{email: "some content", encrypted_password: "some content"}
+  @valid_attrs %{email: "some content", password: "some content"}
   @invalid_attrs %{}
 
   test "changeset with valid attributes" do
