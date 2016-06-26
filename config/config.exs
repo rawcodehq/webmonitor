2fbc73bac86815692752c7b257dc763570844e66
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2016-06-27 00:16:01 +0530

    fix smtp config

--- config/config.exs
+++ config/config.exs
@@ -31,7 +31,8 @@ config :webmonitor, Webmonitor.Mailer,
 
 config :webmonitor,
   # email
-  default_sender: "Webmonitor Notification <noreply@webmonitor.com>"
+  default_sender: "noreply@webmonitorhq.com"
+  #default_sender: {"Webmonitor Notification", "<noreply@webmonitorhq.com>"}
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
