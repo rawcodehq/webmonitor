f669e1d6a2fe624014eca899ef961de63d9c60a8
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2016-07-09 13:24:37 +0530

    tweak for update

--- deploy.sh
+++ deploy.sh
@@ -18,8 +18,18 @@ run() {
   /bin/bash -c "$@"
 }
 
+build() {
+# local --------------------
+# build the release
+echo "building release for version $CURRENT_VERSION ..."
+run "MIX_ENV=prod mix clean --implode"
+run "MIX_ENV=prod mix compile phoenix.digest"
+run "MIX_ENV=prod mix release"
+}
+
 init(){
   # init code
+  build
   echo "initializing remote code"
   echo "copying tarball"
   run "scp ${RELEASE_TAR} ${APP_NAME}@${SERVER_HOST}:${SERVER_TMP_FILENAME}"
@@ -46,6 +56,33 @@ EOS
   # TODO: create the systemctl file
 }
 
+upgrade(){
+  build
+  # upgrade code
+  echo "upgrading remote code"
+  echo "copying tarball"
+  run "scp ${RELEASE_TAR} ${APP_NAME}@${SERVER_HOST}:${SERVER_TMP_FILENAME}"
+
+cat <<EOS | ssh -T "$APP_NAME@$SERVER_HOST" 'cat - > /tmp/deploy.sh; /bin/bash -l /tmp/deploy.sh'
+#!/bin/bash
+
+set -e # fail on first error
+# code that runs on the server
+run() {
+  echo "remote: \$@ ..."
+  /bin/bash -c "\$@"
+}
+# copy the first version of the code
+run "mkdir -p $SERVER_ROOT/releases/$CURRENT_VERSION"
+run "mv $SERVER_TMP_FILENAME $SERVER_ROOT/releases/$CURRENT_VERSION/$APP_NAME.tar.gz"
+# start the app
+cd $SERVER_ROOT
+run "bin/$APP_NAME upgrade $CURRENT_VERSION"
+# make sure it is up by running ping
+run "bin/$APP_NAME ping"
+EOS
+}
+
 help(){
   echo 'Elixir Deployer'
   echo 'Usage:'
@@ -57,6 +94,9 @@ case $1 in
   init )
     init
     ;;
+  upgrade )
+    upgrade
+    ;;
   help )
     help
     ;;
@@ -66,36 +106,5 @@ esac
 
 exit
 
-#TMP_TAR_LOCATION=
-#RELEASE_TAR_LOCATION
-
-
-# local --------------------
-# build the release
-echo "building release for version $CURRENT_VERSION ..."
-run "MIX_ENV=prod mix clean --implode"
-run "MIX_ENV=prod mix compile phoenix.digest"
-run "MIX_ENV=prod mix release"
-# scp tarball to the server
-SCP_DEST="$SERVER:$SERVER_ROOT/tmp/$APP_NAME-$CURRENT_VERSION.tar.gz"
-#SCP_DEST="$SERVER:$SERVER_ROOT/releases/$CURRENT_VERSION/$APP_NAME.tar.gz"
-run "scp $RELEASE_TAR $SCP_DEST"
-
 # remote --------------------
-# run migrations if needed
-# run `app upgrade` if this is an upgrade
-echo "deploying to server"
-cat <<EOS | ssh -T $SERVER 'cat - > /tmp/deploy.sh; /bin/bash -l /tmp/deploy.sh'
-#!/bin/bash
-
-set -e # fail on first error
-# code that runs on the server
-run() {
-  echo "remote: \$@ ..."
-  /bin/bash -c "\$@"
-}
-run "mkdir -p $SERVER_ROOT/releases/$CURRENT_VERSION"
-run "mv $SERVER_ROOT/tmp/$APP_NAME-$CURRENT_VERSION.tar.gz $SERVER_ROOT/releases/$CURRENT_VERSION/$APP_NAME.tar.gz"
-cd $SERVER_ROOT
-run "bin/$APP_NAME upgrade $CURRENT_VERSION"
-EOS
+# TODO: run migrations if needed
