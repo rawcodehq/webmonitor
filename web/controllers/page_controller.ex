037e5c32b151c2fb43162518383bbfee96d97a87
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2016-08-10 13:22:11 +0530

    change home page for authenticated users

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,12 @@
 defmodule Webmonitor.PageController do
   use Webmonitor.Web, :controller
 
-  def index(conn, _params) do
+  def index(%{assigns: %{current_user: :nothing}} = conn, params) do
     render conn, "index.html"
+  end
+
+  def index(conn, params) do
+    put_in(conn.private.phoenix_view, Webmonitor.MonitorView)
+    |> Webmonitor.MonitorController.index(params)
   end
 end
